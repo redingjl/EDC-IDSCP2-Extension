7d72e71c2be779e40b95e4d371eda8d124f09bf5
Jean-Luc Reding <jean-luc.reding@aisec.fraunhofer.de>
2023-06-05 17:15:17 +0200

    Created extension

---
+++ extension/src/main/java/de/fhg/ids/edcidscp2/Idscp2Extension.java
@@ -0,0 +1,8 @@
+package de.fhg.ids.edcidscp2;
+
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+
+@Extension(value = Idscp2Extension.NAME)
+public class Idscp2Extension {
+    public static final String NAME = "IDSCP2 Extension";
+}
