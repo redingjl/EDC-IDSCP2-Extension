561a229b52372d4544c4206b46d07566fe06ffc7
Jean-Luc Reding <jean-luc.reding@aisec.fraunhofer.de>
2023-06-06 11:53:23 +0200

    Added extension body

--- extension/src/main/java/de/fhg/ids/edcidscp2/Idscp2Extension.java
+++ extension/src/main/java/de/fhg/ids/edcidscp2/Idscp2Extension.java
@@ -1,8 +1,24 @@
 package de.fhg.ids.edcidscp2;
 
 import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
 
+/**
+ * Provides support for reading data from an IDSCP2 endpoint and sending data to an IDSCP2 endpoint.
+ */
 @Extension(value = Idscp2Extension.NAME)
-public class Idscp2Extension {
+public class Idscp2Extension implements ServiceExtension {
     public static final String NAME = "IDSCP2 Extension";
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public void initialize(ServiceExtensionContext context) {
+        var monitor = context.getMonitor();
+        monitor.info("###############IDSCP2 Extension started###############");
+    }
 }
